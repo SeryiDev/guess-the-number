6cd81769b611475b3f82ae30d288dafd98c5db82
Sergio García <sergicodcraft@gmail.com>
2022-09-30 13:16:58 +0200

    1st commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,14 @@ export class HomePage {
 
   constructor() {}
 
+  minNumber: number = 0
+  maxNumber: number = 100
+
+  generateRandomNumber(minNumber: number, maxNumber: number) {
+    if(minNumber >= maxNumber) {
+      return 0
+    } else {
+      return Math.floor(Math.random() * (maxNumber - minNumber - 1) + minNumber)
+    }
+  }
 }
